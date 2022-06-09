7de69e44731d56f882d1a7ad443730756ffc12fc
Samed Semih <samedsemihs@gmail.com>
2022-06-09 15:26:50 +0300

    Add files via upload

---
+++ NewtonSoftJson/XmlToJson.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Xml;
+
+namespace XmltoJson
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string xml = @"<?xml version='1.0' standalone='no'?>
+                        <root>
+                          <person id='1'>
+                          <name>Alan</name>
+                          <url>http://www.google.com</url>
+                          </person>
+                          <person id='2'>
+                          <name>Louis</name>
+                          <url>http://www.yahoo.com</url>
+                          </person>
+                        </root>";
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xml);
+
+            string json = JsonConvert.SerializeXmlNode(doc);
+
+            Console.WriteLine(json);
+            // {
+            //   "?xml": {
+            //     "@version": "1.0",
+            //     "@standalone": "no"
+            //   },
+            //   "root": {
+            //     "person": [
+            //       {
+            //         "@id": "1",
+            //         "name": "Alan",
+            //         "url": "http://www.google.com"
+            //       },
+            //       {
+            //         "@id": "2",
+            //         "name": "Louis",
+            //         "url": "http://www.yahoo.com"
+            //       }
+            //     ]
+            //   }
+            // }
+        }
+    }
+}
